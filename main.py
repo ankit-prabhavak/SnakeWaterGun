f566a5c6be264bb6a57a422b1daa448f8d703ee5
ankit-prabhavak <ankitabcd1718@gmail.com>
2024-12-28 19:06:37 +0530

    updated main.py

--- main.py
+++ main.py
@@ -3,9 +3,6 @@
 import random
 import pyttsx3
 
-computer=random.choice([1,-1,0])
-
-
 engine = pyttsx3.init()
 engine.setProperty('rate', 150)
 engine.setProperty('voice', "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_ZIRA_11.0")
